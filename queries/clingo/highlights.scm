0b635005b586eca15a5e8250daef76ae211abf80
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-11-26 01:22:26 +0100

    add some more highlights

--- queries/clingo/highlights.scm
+++ queries/clingo/highlights.scm
@@ -1,15 +1,70 @@
-(aggregatefunction)  @preproc
-(VARIABLE)  @identifier
-(single_comment)  @comment
-(multi_comment)  @comment
-(NUMBER)  @number
-(STRING)  @string
+; comments
+(single_comment) @comment
+(multi_comment) @comment
+
+; identifiers
+(VARIABLE) @parameter
+(identifier) @identifier
+
+; constants
+(NUMBER) @number
+(STRING) @string
+
+; meta directives
+(statement (INCLUDE) @include)
+(statement (CONST) @define)
+(statement (DEFINED) @preproc)
+
+; delimiters
+(cmp) @delimiter
+(IF) @delimiter
+(WIF) @delimiter
+(SEM) @delimiter
+(COMMA) @delimiter
+(DOT) @delimiter
+(AT) @delimiter
+(COLON) @delimiter
+
+; punctuation
+(LPAREN) @punctuation.bracket
+(RPAREN) @punctuation.bracket
+(LBRACE) @punctuation.bracket
+(RBRACE) @punctuation.bracket
+(LBRACK) @punctuation.bracket
+(RBRACK) @punctuation.bracket
+
+; operators
+(ADD) @operator
+(SLASH) @operator
+(SUB) @operator
+(MUL) @operator
+(MOD) @operator
+(POW) @operator
+(DOTS) @operator
+(QUESTION) @operator
+(BNOT) @operator
+(AND) @operator
+(NOT) @operator
+(EQ) @operator
+
+; atoms and some terms
+(atom (identifier) @function)
+(term (identifier) @constant) 
+(term (AT) (identifier)) @function.builtin
+
+; theory terms
+; ...
+
+; keywords
+(aggregatefunction) @keyword
+(theory_atom
+    (AND)@keyword
+    (theory_atom_name
+    (identifier) @keyword))
 (literal (TRUE) @keyword)
 (literal (FALSE) @keyword)
-(cmp) @special
-(IF) @special
-(WIF) @special
-(theory_atom
-    (AND)@preproc
-    (theory_atom_name
-    (identifier)@preproc))
+(statement (SHOW) @keyword)
+(statement (EXTERNAL) @keyword)
+(statement (MINIMIZE) @keyword)
+(statement (MAXIMIZE) @keyword)
+(statement (PROJECT) @keyword)
